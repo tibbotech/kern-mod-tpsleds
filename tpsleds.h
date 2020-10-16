f9f81c1be20be2f2210b2693960315467f95240f
Dvorkin Dmitry <dvorkin@tibbo.com>
2020-10-16 17:19:29 +0300

    Tiny update

--- tpsleds.h
+++ tpsleds.h
@@ -6,7 +6,7 @@
 #define M_AUT "Dvorkin Dmitry dvorkin@tibbo.com"
 #define M_NAM "Tibbo TPSLeds module"
 #define M_ORG "Tibbo Tech."
-#define M_CPR "(C) 2015-2016"
+#define M_CPR "(C) 2020"
 
 #define TPS_MAX_LEDS 5
 
@@ -21,15 +21,5 @@
 #define DV_BLED_FT 57
 
 void tps_leds_set( unsigned char _mask);
-
-//struct ninja_skbq_T {
-// spinlock_t my_spinlock;
-// // current queue len
-// ulong q_len;
-// // number of dropped packets because of the size
-// ulong q_dropped;
-// struct sk_buff_head skbs;
-//};
-//typedef struct ninja_skbq_T ninja_skbq_t;
 
 #endif // TPSLEDS_H
